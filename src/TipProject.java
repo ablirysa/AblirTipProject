cf95fef2f63efa49d614bb968da21dd6f4f7bcb4
ablirysa <ysabellea@nycstudents.net>
2022-09-28 09:09:33 -0400

    changed the variables/math

--- src/TipProject.java
+++ src/TipProject.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class TipProject {
+import java.util.Scanner;
+
+public class TipProject {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Total Bill: ");
+        double bill = scan.nextDouble();
+        System.out.println();
+
+        System.out.print("Tip Percentage (as a whole number): ");
+        int tipPercent = scan.nextInt();
+        System.out.println();
+
+        System.out.print("Number of People: ");
+        int people = scan.nextInt();
+        System.out.println();
+
+        double tipPayed = (double) tipPercent/100 * bill;
+        double total = bill + tipPayed;
+        double perPerson = total/people;
+        double tipPerPerson = tipPayed / people;
+        System.out.print("In this scenario, the total would be $" + total);
+        System.out.print(" and each person is going to pay $" + perPerson);
+        System.out.println(" with the tip per person being $" + tipPerPerson);
+    }
 }
