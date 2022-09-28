ef5f3ff68c025810f6a9dbff84e55876e1d0c997
ablirysa <ysabellea@nycstudents.net>
2022-09-28 09:26:39 -0400

    comments

--- src/TipProject.java
+++ src/TipProject.java
@@ -4,24 +4,30 @@ public class TipProject {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Obtaining values from the user
         System.out.print("Total Bill: ");
         double bill = scan.nextDouble();
         System.out.println();
-
         System.out.print("Tip Percentage (as a whole number): ");
         int tipPercent = scan.nextInt();
         System.out.println();
-
         System.out.print("Number of People: ");
         int people = scan.nextInt();
         System.out.println();
 
-        double tipPayed = (double) tipPercent/100 * bill;
+        // Math
+        double tipPayed = (double) tipPercent/100 * bill; // had to cast tipPercent or 100 in order to get intended result
         double total = bill + tipPayed;
         double perPerson = total/people;
         double tipPerPerson = tipPayed / people;
-        System.out.printf("In this scenario, the total would be $" + "%.2f", + total);
-        System.out.printf(" and each person is going to pay $" + "%.2f", + perPerson);
-        System.out.printf(" with the tip per person being $" + "%.2f", + tipPerPerson);
+
+        // Printed Statements informing User
+        System.out.printf("Total: $" + "%.2f", + total);
+        System.out.println();
+        System.out.printf("Total Tip: $" + "%.2f", + tipPayed);
+        System.out.println();
+        System.out.printf("Total (per person): $" + "%.2f", + perPerson);
+        System.out.println();
+        System.out.printf("Total Tip (per person):" + "%.2f", + tipPerPerson);
     }
 }
